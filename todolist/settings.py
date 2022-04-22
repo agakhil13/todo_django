0ff695e643b8828fe40f3ceff35937ee88c037af
agakhil13 <aguptaji13101@gmail.com>
2022-04-22 14:55:57 +0530

    final application

--- todolist/settings.py
+++ todolist/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = 'django-insecure-=@!sk^r0fj7jajg_)l5n4nk6o=sg&ue7fba&xwclu7r6+s#6u^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -78,15 +77,15 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
